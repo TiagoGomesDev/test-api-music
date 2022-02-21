fd85c6251f46758c9726d7040a08b7b6f95346d5
Tiago Luis Gomes Santos <tiago.santos@intranet.fapesb.ba.gov.br>
2022-02-21 14:14:42 -0300

    commit inicial

---
+++ assets/js/auth.js
@@ -0,0 +1,20 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'));
+
+//Tratamento para tentativa de acesso sem login
+if(localStorage.getItem('token') === null){
+  alert("Você precisa estar logado!!");
+  window.location.href = 'index.html';
+
+}
+
+//Limpando do localStorage os itens temporários ao clicar no botão Sair. 
+const sair = () =>{
+  localStorage.removeItem('token');
+  localStorage.removeItem('userLogado');
+  window.location.href = 'index.html';
+}
+
+ let boasVindas = document.querySelectorAll('.boas-vindas');
+//  console.log(boasVindas);
+ boasVindas.innerHTML = `<p>${userLogado.nome}</p>`
+//  boasVindas.innerHTML = `<p>Bem vindo, ${userLogado.nome}!! Em breve você poderá acessar nosso dashboard para aproveitar ao máximo nossos serviços.</p>`
---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+let verSenha = document.querySelector("#verSenha");
+
+verSenha.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#senha');
+  if(inputSenha.getAttribute('type') === 'password'){
+    inputSenha.setAttribute('type', 'text')
+  }else{
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+const entrar = () =>{
+  let user = document.querySelector('#user');
+  let labelUser = document.querySelector('#labelUser');
+
+  let senha = document.querySelector('#senha');
+  let labelSenha = document.querySelector("#labelSenha");
+
+  let msgError = document.querySelector(".msgError");
+
+
+  let lista_user = [];
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
+
+  lista_user = JSON.parse(localStorage.getItem('lista_user'))
+  
+  //Verifica se o usuário e senha digitados existem no localStorage
+  lista_user.forEach((item) => {
+    if(user.value === item.user && senha.value === item.senha){
+      userValid = {
+        nome: item.nome,
+        user: item.user,
+        senha: senha.value,
+      }
+    }
+  })
+
+  const errorAuth = () => {
+    labelUser.setAttribute('style', 'color: red');
+    user.setAttribute('style', 'border-color: red');
+    labelSenha.setAttribute('style', 'color: red');
+    senha.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block');
+    msgError.innerHTML = '<p>Usuário e/ou senha incorretos.</p>'
+    user.focus();
+  }
+
+  //tratamento para login
+  if(user.value === userValid.user && senha.value === userValid.senha){
+    window.location.href = 'menu.html';
+    msgError.setAttribute('style', 'display: none');
+    //Atribuindo token de validação
+    let token = Math.random().toString(16).substring(2);
+    localStorage.setItem('token', token);
+    //Passando informações de quem está logando para o localStorage
+    localStorage.setItem('userLogado', JSON.stringify(userValid));
+  }else{
+    errorAuth();
+  }
+}
+
