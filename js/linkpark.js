fae90c22dc722a9524a9335b316698b7bc591c80
TiagoGomesDev <tiago.santos@unidompedro.com>
2022-03-05 12:13:25 -0300

    pag rock

---
+++ js/linkpark.js
@@ -0,0 +1,115 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+  //   name: "Harley Bird - Home",
+  //   artist: "Jordan Schor",
+  //   img: "music-1",
+  //   src: "music-1"
+  // },
+  // {
+  //   name: "Ikson Anywhere – Ikson",
+  //   artist: "Audio Library",
+  //   img: "music-2",
+  //   src: "music-2"
+  // },
+  // {
+    name: "Until It Breaks",
+    artist: "Link Park",
+    img: "music-1",
+    src: "10 - Until It Breaks"
+  },
+  {
+    name: "Burn It Down",
+    artist: "Link Park",
+    img: "music-2",
+    src: "Burn It Down"
+  },
+  {
+    name: "Castle Of Glass",
+    artist: "Link Park",
+    img: "music-4",
+    src: "Castle Of Glass"
+  },
+  {
+    name: "In My Remains",
+    artist: "Link Park",
+    img: "music-5",
+    src: "In My Remains"
+  },
+  {
+    name: "Jornada del Muerto",
+    artist: "Link Park",
+    img: "music-6",
+    src: "Jornada del Muerto"
+  },
+  {
+    name: "Lies Greed Misery",
+    artist: "Link Park",
+    img: "music-6",
+    src: "Lies Greed Misery",
+  }, 
+  {
+    name: "In The End",
+    artist: "Link Park",
+    img: "music-6",
+    src: "Link Park -in the end",
+  }, {
+    name: "Lost In The Echo",
+    artist: "Link Park",
+    img: "music-6",
+    src: "Lost In The Echo",
+  },
+  {
+    name: "LP Waiting For The End",
+    artist: "Link Park",
+    img: "music-6",
+    src: "LP Waiting For The End",
+  },
+  {
+    name: "Powerless",
+    artist: "Link Park",
+    img: "music-3",
+    src: "POWERLESS"
+  },
+  {
+    name: "Roads Untraveled",
+    artist: "Link Park",
+    img: "music-3",
+    src: "ROADS UNTRAVELED"
+  },
+  {
+    name: "Skin To Bone",
+    artist: "Link Park",
+    img: "music-3",
+    src: "Skin To Bone"
+  },
+  {
+    name: "Tinfoil",
+    artist: "Link Park",
+    img: "music-3",
+    src: "TINFOIL"
+  },
+  {
+    name: "Victimized",
+    artist: "Link Park",
+    img: "music-3",
+    src: "VICTIMIZED"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
