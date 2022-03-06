ccc2317d00986f6dc19246fefdbeb7294071d8aa
TiagoGomesDev <tiago.santos@unidompedro.com>
2022-03-06 09:27:34 -0300

    pag system/theb

---
+++ js/systemofdown.js
@@ -0,0 +1,215 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+    name: "attack",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "attack"
+    
+    // name: "Luis Thug",
+    // path: "songs/LuisCarlos/l1.mpeg",
+    //   img: "img/Cantores/LuisCarlos.jpeg",
+  },
+  {
+    name: "Prision Song",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "01 - Prision Song"
+  },
+  {
+    name: "Needles",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "02 - Needles"
+  },  {
+    name: "Deer Dance",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "03 - Deer Dance"
+  },
+  {
+    name: "Jet Pilot",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "04 - Jet Pilot"
+  },  {
+    name: "X",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "05 - X"
+  },
+  {
+    name: "Chop Suey!",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "06 - Chop Suey!"
+  },  {
+    name: "Bounce",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "07 - Bounce"
+  },
+  {
+    name: "Needles",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "02 - Needles"
+  },  {
+    name: "Atwa",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "09 - Atwa"
+  },
+  {
+    name: "Science",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "10 - Science"
+  },  {
+    name: "Toxicity",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "12 - Toxicity"
+  },
+  {
+    name: "Psycho",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "13 - Psycho"
+  },  {
+    name: "Aerials",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "14 - Aerials"
+  },
+  {
+    name: "Arto",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "15 - Arto"
+  },  {
+    name: "Baby One More Time",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "system of a down - Baby One More Time"
+  },
+  {
+    name: "Boom",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "System of a Down - Boom"
+  },  {
+    name: "Byob",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "system of a down - byob"
+  },
+  {
+    name: "I-E-A-I-A-I-O",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "System of a Down - I-E-A-I-A-I-O (vmuzike.net)"
+  },  {
+    name: "Prision Song",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "01 - Prision Song"
+  },
+  {
+    name: "Needles",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "02 - Needles"
+  },  {
+    name: "Jet Pilot",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "System of a Down - Jet Pilot"
+  },
+  {
+    name: "Needles",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "02 - Needles"
+  },  {
+    name: "Prision Song",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "01 - Prision Song"
+  },
+  {
+    name: "Needles",
+    artist: "System of a Down",
+    img: "13953133848_face7c3844",
+    src: "02 - Needles"
+  },
+  // {
+  //   name: "Ikson Anywhere – Ikson",
+  //   artist: "Audio Library",
+  //   img: "music-2",
+  //   src: "music-2"
+  // },
+  // {
+  //   name: "Beauz & Jvna - Crazy",
+  //   artist: "Beauz & Jvna",
+  //   img: "music-3",
+  //   src: "music-3"
+  // },
+  // {
+  //   name: "Hardwind - Want Me",
+  //   artist: "Mike Archangelo",
+  //   img: "music-4",
+  //   src: "music-4"
+  // },
+  // {
+  //   name: "Jim - Sun Goes Down",
+  //   artist: "Jim Yosef x Roy",
+  //   img: "music-5",
+  //   src: "music-5"
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "music-6"
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "KalebJoshCéueTerraseEncontram128kbps",
+  // }, 
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "X2Download.com - Emaús - Kaleb e Josh [Home Sesssons] Morada (128 kbps)",
+  // }, {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "y2meta.com - Kaleb e Josh - Céu e Terra se Encontram (Vídeo Oficial) (64 kbps)",
+  // }, {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "KalebJoshCéueTerraseEncontram128kbps",
+  // },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
